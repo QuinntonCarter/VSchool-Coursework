ecd380e891605438cd06a85f818cd787dcba27db
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-06-25 16:30:53 -0600

    started functions in Node Calc

---
+++ Classwork/module1/Node Calc/app.js
@@ -0,0 +1,39 @@
+function erase(){
+
+}
+
+function add(){
+
+}
+
+function sub(){
+
+}
+
+function mult(){
+
+}
+
+function div(){
+
+}
+
+function div(){
+
+}
+
+function equals(){
+
+}
+
+var submitDisplay = function(clickButton){
+    
+}
+
+for (let i = 0; i <mid-integer.length; i++){
+    mid-integer[i].addEventListener('click', submitDisplay, false)
+}
+
+for (let i = 0; i <operand.length; i++){
+    operand[i].addEventListener('click', operation, false)
+}
