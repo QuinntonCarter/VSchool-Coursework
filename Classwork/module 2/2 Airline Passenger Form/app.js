72ec549e701e498cef29c9ec5ecb8b658b7de44b
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-07-01 16:17:48 -0600

    need to finish option list collection for alert

--- Classwork/module 2/2 Airline Passenger Form/app.js
+++ Classwork/module 2/2 Airline Passenger Form/app.js
@@ -6,18 +6,47 @@ form.addEventListener("submit", function(e){
     const firstNameCollect = form.firstname.value
     const lastNameCollect = form.lastname.value
     // collected full name var
-    var fullName = firstNameCollect + lastNameCollect
-    // how to collect radio selection
-    const genderCollect = form.gender
-    // how to collect radio selection
-    const dietCollect = form.diet
+    var fullName = firstNameCollect +" "+ lastNameCollect
+    // collects radio selection
+    const genderCollect = form.gender.value
+    // collects checkbox selections
+    const dietCollect = [];
+    const dietSelect = document.querySelectorAll("input[name=diet]:checked");
+    for (var i=0; i < dietSelect.length; i++) {
+        dietCollect.push(dietSelect[i].value);
+    }
     // reg text inputs
     const otherCollect = form.other.value
     const ageCollect = form.age.value
     // how to collect option/select selection
-    const destinationCollect = form.destination.option
 
-    const passengerInformation = fullName+\n+genderCollect+\n+dietCollect+\n+otherCollect+\n+ageCollect+\n+destinationCollect+\n+"Thank you "+fullName+" for choosing Mile High!"
+
+    // const passengerInformation = fullName+\n+genderCollect+\n+dietCollected+\n+otherCollect+\n+ageCollect+\n+destinationCollect+\n+"Thank you "+fullName+" for choosing Mile High!"
     
-    alert(passengerInformation)
+    alert(destination)
 })
+
+// ----------------------------------------- //
+// reference code for checked/radio
+document.animals.addEventListener("submit", function(e){
+    e.preventDefault()
+    var checkedAnimals = [];
+    var checkedBoxes = document.querySelectorAll("input[name=likes]:checked");
+    for (var i = 0; i < checkedBoxes.length; i++) {
+        checkedAnimals.push(checkedBoxes[i].value);
+    }
+    console.log(checkedAnimals)
+});
+
+// 2
+// document.animals.addEventListener("submit", function(e){
+//     e.preventDefault()
+//     var animals = document.animals.likes
+//     var checkedAnimals = []
+//     for (var i = 0; i < animals.likes.length; i++) {
+//     if(animals[i].checked){
+//         checkedAnimals.push(animals[i].value)
+//     }
+// }
+//     console.log(checkedAnimals)
+// })
