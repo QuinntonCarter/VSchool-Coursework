7a963f157bb185612b1b395888e29f107f82016c
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-07-02 21:32:20 -0600

    returning undefined at total

--- Classwork/module 2/3 Mario Pest Control/app.js
+++ Classwork/module 2/3 Mario Pest Control/app.js
@@ -20,15 +20,13 @@ const bobombClick = document.getElementsByClassName("Bobomb")
 // })
 // need to figure out how to multiply values and return them to form to be added together
 function add(x,y,z){
-    let first = parseInt(x)
-    let second = parseInt(y)
-    let third = parseInt(z)
-    sum = first+second+third
-    alert(sum)
+    addsum = x+y+z
+
 }
 
 function mult(x,y){
-    sum = x * y
+    multsum = x * y
+
 }
 
 
@@ -36,38 +34,39 @@ form.addEventListener("submit", function(e){
     e.preventDefault()
 
     // variable for value of goomba checkbox (need to parse to int)
-    // const goomba = form.goombacheck.value
+    const goomba = form.goombacheck.value
     // // turn goomba base val into int
-    // const goombasBaseVal = parseInt(goomba)
+    const goombasBaseVal = parseInt(goomba)
 
     // // variable for value of cheeps checkbox (need to parse to int)
-    // const cheeps = form.cheepcheck.value
+    const cheeps = form.cheepcheck.value
     // // turn cheep base val to integer
-    // const cheepsBaseVal = parseInt(cheeps)
+    const cheepsBaseVal = parseInt(cheeps)
     
     // // variable for value of bobombs checkbox (need to parse to int)
-    // const bobombs = form.bobombcheck.value
+    const bobombs = form.bobombcheck.value
     // // turn bobomb base val into int
-    // const bobombBaseVal = parseInt(bobombs)
+    const bobombBaseVal = parseInt(bobombs)
     // // ---  
     // // variable for customers input value of goombas
-    // const custGoombas = form.customergoombas.value
-    // const goombaCustInt = parseInt(custGoombas)
+    const custGoombas = form.customergoombas.value
+    const goombaCustInt = parseInt(custGoombas)
 
     // // variable for customers input value of cheepcheeps (need to parse to int)
-    // const custCheeps = form.customercheeps.value
-    // const cheepsCustInt = parseInt(custCheeps)
+    const custCheeps = form.customercheeps.value
+    const cheepsCustInt = parseInt(custCheeps)
 
     // // // variable for customers input value of bobombs (need to parse to int)
-    // const custBobombs = form.customerbobombs.value
-    // const custBobombsInt = parseInt(custBobombs)
+    const custBobombs = form.customerbobombs.value
+    const custBobombsInt = parseInt(custBobombs)
     // // ---  
     // // const totalReturn = document["totalreturn"]
-    // const totalGoombas = mult(goombasBaseVal,goombaCustInt)
-    // const totalCheeps = mult(cheepsBaseVal,cheepsCustInt)
-    // const totalBobombs = mult(bobombBaseVal,custBobombsInt)
+    const totalGoombas = mult(goombasBaseVal,goombaCustInt)
+    const totalCheeps = mult(cheepsBaseVal,cheepsCustInt)
+    const totalBobombs = mult(bobombBaseVal,custBobombsInt)
     
-    // const total = add(totalGoombas,totalCheeps,totalBobombs)
+    const total = add(totalGoombas,totalCheeps,totalBobombs)
+    alert(total)
 
 })
 
