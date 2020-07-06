37f05c62ddace643a33fddf17ddfd041c6c91d5c
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-07-06 17:14:56 -0600

    messing with set/clearinterval

--- Classwork/module 2/4 Speed Clicker!/app.js
+++ Classwork/module 2/4 Speed Clicker!/app.js
@@ -5,8 +5,12 @@ var savedCount = []
 var count= 0
 
 
-document.body.addEventListener("click", function(e){
+document.body.addEventListener("click", function(){
     e.preventDefault(e)
+    // need to fix so it doesn't prevent page reload
+    // setTimeout(function(){ 
+    //     alert("Time's up! Great work! \n Reload the page to try again.");
+    // }, 30000)
     // increments var count by one and returns result //
     let counted = ++count
     // assigns count var to new div element //
---
+++ Classwork/module 2/6 Array Filter Exercises/app.js
@@ -0,0 +1,42 @@
+let arr = [1,2,3,4,5]
+let words = ['cat','bird','naruto','akira']
+
+// 1) Given an array of numbers, 
+// return a new array that has only the numbers that are 5 or greater.
+
+// const result = arr.filter(function(num){
+// if(num >= 5){
+//     return num
+// }
+// })
+// console.log(result)
+
+// 2) Given an array of numbers, 
+// return a new array that only includes the even numbers.
+
+// const result = arr.filter(function(num){
+//     if(num % 2 === 0){
+//         return num
+//     }
+// })
+// console.log(result)
+
+// 3) Given an array of strings, return a new array that 
+// only includes those that are 5 characters or fewer in length
+
+const result = words.filter(function(words){
+    if(){
+
+    }
+})
+
+
+// 4) Given an array of people objects, return a new array 
+// that has filtered out all those who don't belong to the club.
+
+
+
+// 5) Make a filtered list of all the 
+// people who are old enough to see The Matrix (older than 18)
+
+
