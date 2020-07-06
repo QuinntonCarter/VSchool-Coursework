a3312430754e33fbe3d597397da08f4a1f8963b4
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-07-05 21:49:42 -0600

    Array map exercises

--- Classwork/module 2/4 Speed Clicker!/app.js
+++ Classwork/module 2/4 Speed Clicker!/app.js
@@ -1,5 +1,7 @@
+// sessionStorage.setItem("savedCount")
 const counter = document["counthere"]
 const newdiv = document.createElement("div")
+var savedCount = []
 var count= 0
 
 
@@ -7,9 +9,9 @@ document.body.addEventListener("click", function(e){
     e.preventDefault(e)
     // increments var count by one and returns result //
     let counted = ++count
-    
     // assigns count var to new div element //
     newdiv.textContent = counted
+    savedCount.push(counted)
     // appends new div w/ count value to 'counthere' div //
     counter.append(newdiv)
 })
---
+++ Classwork/module 2/5 Array Map Exercises/app.js
@@ -0,0 +1,62 @@
+let arr = [1,2,3]
+let names = ['preston','christian','steve']
+let people = [{
+    name: 'Mauricio',
+    occupation: 'Pharmacist',
+    age: 31
+},
+{
+    name:'Christian',
+    occupation:'Technical Support Specialist',
+    age: 28
+},
+{
+    name:'Preston',
+    occupation:'Super Dad',
+    age: 31
+},
+{
+    name:'Steve',
+    occupation:'EMT',
+    age: 31
+}]
+
+// 1) Make an array of numbers that are doubles of the first array
+// let dbl = arr.map(num => num * 2)
+
+// console.log(dbl)
+
+
+// 2) Take an array of numbers and make them strings
+// let stringer = arr.map(num => JSON.stringify(num))
+
+// console.log(stringer)
+
+// 3) Capitalize each of an array of names
+// let capper = names.map(function(word){
+//     JSON.stringify(word)
+//     return word.toUpperCase()
+// })
+// console.log(capper)
+
+// 4) Make an array of strings of the names
+// let namearr = people.map(function(people){
+//     return people.name
+// })
+// console.log(namearr)
+
+// ****5) Make an array of strings of the names saying whether or not they can go to The Matrix
+// let yesMatrix = []
+
+// let matrix = people.map(function(people){
+//     if (people.age > 18){
+//         yesMatrix.push(people.name)
+//     } else {
+//         console.log(people.name+"can't go")
+//     }
+// })
+// console.log(yesMatrix)
+
+
+// ****6) Make an array of the names in h1s, and the ages in h2s
+// I don't understand the wording of this question.. push them to h1 and h2 divs?
