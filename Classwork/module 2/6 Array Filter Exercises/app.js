f9a8b8b7f74a094e62179185ded41c3c49fb7332
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-07-06 18:05:06 -0600

    finished array filter exercises

--- Classwork/module 2/6 Array Filter Exercises/app.js
+++ Classwork/module 2/6 Array Filter Exercises/app.js
@@ -1,5 +1,33 @@
-let arr = [1,2,3,4,5]
-let words = ['cat','bird','naruto','akira']
+let arr = [1,2,3,4,5];
+let words = ['cat','bird','naruto','akira'];
+let people = [
+    {
+    name: "Drake",
+    inthaclub: true,
+    age: 15
+    },
+    {
+        name: "Kevin Gates",
+        inthaclub: true,
+        age: 17
+    },
+    {
+        name: "noname",
+        inthaclub: false,
+        age: 22
+    },
+    {
+        name: "Denzel Curry",
+        inthaclub: false,
+        age: 24
+    },
+    {
+        name: "Father",
+        intheclub: false,
+        age: 25
+    }
+]
+
 
 // 1) Given an array of numbers, 
 // return a new array that has only the numbers that are 5 or greater.
@@ -24,19 +52,29 @@ let words = ['cat','bird','naruto','akira']
 // 3) Given an array of strings, return a new array that 
 // only includes those that are 5 characters or fewer in length
 
-const result = words.filter(function(words){
-    if(){
-
-    }
-})
-
+// const result = words.filter(function(words){
+//     if(words.length <= 5){
+//         return words
+//     }
+// })
+// console.log(result)
 
 // 4) Given an array of people objects, return a new array 
 // that has filtered out all those who don't belong to the club.
-
-
+//     const result = people.filter(function(people){
+//         if(people.inthaclub === true){
+//             return people.name
+//         } else {
+//             console.log(people.name+" isn't in the club..")
+//         }
+//     })
+// console.log(result)
 
 // 5) Make a filtered list of all the 
 // people who are old enough to see The Matrix (older than 18)
-
-
+// const result = people.filter(function(people){
+//     if (people.age >= 17){
+//         return people.name
+//     }
+// })
+// console.log(result)
