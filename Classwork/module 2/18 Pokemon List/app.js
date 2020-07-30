776295050cdea7dde080e28354763b6fd1cfc573
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-07-30 14:27:37 -0600

    poke list

--- Classwork/module 2/18 Pokemon List/app.js
+++ Classwork/module 2/18 Pokemon List/app.js
@@ -1,5 +1,5 @@
 // https://api.vschool.io/pokemon
-// -- Step one - get the data - Write a function that gets the JSON and parses the JSON 
+// -- Step 1 - get the data - Write a function that gets the JSON and parses the JSON 
 // to create an array of objects that look like this:
 
 // {
@@ -12,24 +12,30 @@
 // ...
 // etc.
 
-// -- Step two - display the data - Make each Pokemon's name show up on a separate line in the browser.
+// -- Step 2 - display the data - Make each Pokemon's name show up on 
+// a separate line in the browser.
 // You will be using a for loop to iterate through each pokemon object, 
 // and some DOM mamnipulation to add nodes for each Pokemon.
 
-
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        const jsonData = xhr.responseText;
-        const data = JSON.parse(jsonData);
-        const pokemon = data.pokemon;
-        document.getElementsByClassName("here").textContent = "Name: "+jsonData+"\nResource_URI: "+data.name;
-        // document.getElementsByClassName("demo2").append();
-        pokeUrl = data.pokemon[0];
-        console.log(pokeUrl);
-    }
-};
 
 xhr.open("GET", "https://api.vschool.io/pokemon", true);
 xhr.send();
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        const JSONData = xhr.responseText
+        const data = JSON.parse(JSONData)
+        showData(data.results)
+    }
+}
+
+function showData(arr){
+    const h1 = document.createElement('h1')
+    h1.textContent = poke.name
+    document.body.appendChild(h1)
+}
+
+
+// document.getElementsByClassName("here").textContent = "Name: "+jsonData+"\nResource_URI: "+data.name;
