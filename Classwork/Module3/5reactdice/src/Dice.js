e20b258812d8a9226e0ff6093e707bbd715fe9dd
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-10-12 11:53:28 -0600

    finished react dice

--- Classwork/Module3/5reactdice/src/Dice.js
+++ Classwork/Module3/5reactdice/src/Dice.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import Roll from './Roll'
 
+class Dice extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            roll: 6
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
 
+    handleClick(){
+        let max = 6
+        let sum = Math.floor((Math.random() * max) + 1)
+        this.setState({
+            roll: sum
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <Roll handleClick={this.handleClick} display={this.state.roll} />
+            </div>
+        )
+    }
+}
 
 export default Dice;
--- Classwork/Module3/5reactdice/src/Roll.js
+++ Classwork/Module3/5reactdice/src/Roll.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import './index.css'
 
-
+function Roll(props){
+    // let sum = Math.floor(Math.random * 6)
+    return (
+        <div className='dicedisplay'>
+            <h1 className='dice'>
+                {props.display}
+            </h1>
+            <p>Click2Roll</p>
+            <button onClick={props.handleClick}/> 
+        </div>
+    )
+}
 
 export default Roll;
