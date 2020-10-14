4e5c9ce9f122b82cfa85093ae92cb278c47f8e0a
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-10-14 13:45:33 -0600

    working name badge

---
+++ Classwork/Module3/7namebadge/src/NameBadge.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import UserDetails from './UserDetails';
+
+class NameBadge extends Component{
+    constructor() {
+        super()
+        this.state = {
+
+        }
+
+    }
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default NameBadge;
