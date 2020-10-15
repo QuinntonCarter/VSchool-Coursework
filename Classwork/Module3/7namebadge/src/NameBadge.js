798910aef48c137a60c1d2fda17956e5b1598146
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-10-15 09:34:41 -0600

    finished namebadge

--- Classwork/Module3/7namebadge/src/NameBadge.js
+++ Classwork/Module3/7namebadge/src/NameBadge.js
@@ -19,6 +19,8 @@ class NameBadge extends Component{
     handleSubmit = (e) => {
         e.preventDefault()
         this.setState(prevState => ({
+            // gathers prev state and current usercollected data for
+            // return/rendering of badge. prepends new data in front of old
             userCollected: [
                 prevState, ...this.state.userCollected
                 ],
@@ -39,6 +41,12 @@ class NameBadge extends Component{
                 [name]: value
             }
         )
+    }
+
+    handleTel = (e) => {
+            if(e.target.value === Number){
+                console.log('working')
+            }
     }
 
     render(){
@@ -68,12 +76,12 @@ class NameBadge extends Component{
                     <input className='form' type='email' minLength='3' placeholder='Email' name='email' value={this.state.email} onChange={this.handleChange} required/>
                     <input className='form' type='text' minLength='3' placeholder='Birthplace' name='birthplace' value={this.state.birthplace} onChange={this.handleChange} required/>
                     <br/>
-                    <input className='form' type='tel' minLength='3' maxLength='10' placeholder='Phone Number' name='phone' value={this.state.phone} onChange={this.handleChange} required/>
+                    <input className='form' pattern="[0-9]{3}[0-9]{3}[0-9]{4}" type='tel' minLength='3' maxLength='13' placeholder='Phone Number' name='phone' value={this.state.phone} onChange={this.handleChange} required/>
                     <input className='form' type='text' minLength='3' placeholder='Favorite Food' name='favfood' value={this.state.favfood} onChange={this.handleChange} required/>
                     <br/>
                     <textarea className='form' maxLength='125' placeholder='About Yourself' name='about' value={this.state.about} onChange={this.handleChange} required/>
                     <br/>
-                    <button> Generate Badge </button>
+                    <button className='button'> Generate Badge </button>
                 </form>
             </div>
         {mappedDetails}
