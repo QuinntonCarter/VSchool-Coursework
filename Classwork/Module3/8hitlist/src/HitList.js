f8c294451bf4c689276e338813134565c607f896
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-10-16 15:40:19 -0600

    hitlist

--- Classwork/Module3/8hitlist/src/HitList.js
+++ Classwork/Module3/8hitlist/src/HitList.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function HitList(){
+class HitList extends Component {
+    constructor(){
+        super()
+        this.state = {
+            targets: { }
+            // whacked: {}
+        }
+    }
 
-    return(
-        <div>
-            
-        </div>
-    )
+    // ** doesn't work ** //
+    //     componentDidMount() {
+    //         fetch("https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json")
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             data.map(target =>
+    //                 this.setState({
+    //                     targets: target.name
+    //                 }))
+    //     })
+    // }
+
+        componentDidMount() {
+            fetch("https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json")
+            .then(response => response.json())
+            .then(data => {
+                data.map(target => (
+                    this.setState({
+                        targets: target.name
+                    })
+                )
+                )}
+            )}
+
+    render(){
+        // ** doesn't work ** //
+        // const mappedTargets = this.state.targets.map(target =>
+        //         <div>
+        //             <ul>
+        //                 <li>
+        //                     Target: {target.name}
+        //                 </li>
+        //             </ul>
+        //         </div>
+        //     )
+        return(
+            <div>
+                <h4>
+                    {this.state.targets}
+                </h4>
+                <h6>
+                    {this.state.whacked}
+                </h6>
+            </div>
+        )
+    }
 }
 
 export default HitList;
