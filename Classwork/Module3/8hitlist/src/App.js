f5209fa2e691aad7e1d378ca144c61a93011dcd7
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-10-16 12:02:04 -0600

    workin on hitlist

---
+++ Classwork/Module3/8hitlist/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HitList from 'HitList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <HitList/>
+    </div>
+  );
+}
+
+export default App;
