fe7de35372fb129a6b8d038979548d7f3f63a958
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-10-23 14:34:25 -0600

    finished Big Time DJ Checkpoint

--- Classwork/Module3/4dj/src/PadContainer.js
+++ Classwork/Module3/4dj/src/PadContainer.js
@@ -10,6 +10,7 @@ class PadContainer extends Component {
         }
     }
     
+    ////// //////// * /////// ///////
     bwClick = () => {
         // if condition is met..
         if(this.state.colors[0] === 'white' || this.state.colors[0] === 'gray'){
@@ -18,7 +19,7 @@ class PadContainer extends Component {
         // following array (black)
                 colors: ['black', 'black', 'black', 'black']
             })
-        // but if condition is met
+        ////// but if condition is met
         } if(this.state.colors[0] === 'black' || this.state.colors[0] === 'purple'){
         // sets state to following array (white)
             this.setState({
@@ -27,12 +28,13 @@ class PadContainer extends Component {
         }
     }
 
+    ////// //////// * /////// ///////
     purpClick = () => {
         this.setState({
         //sets colors the included array
             colors: ['purple', 'purple', 'gray', 'gray']
             })
-        // but if this.state.colors[0] matches purple then..
+        ///// but if this.state.colors[0] matches purple then..
         if (this.state.colors[0] === 'purple'){
             this.setState({
         //resets colors the included array
@@ -41,6 +43,7 @@ class PadContainer extends Component {
         }
     }
 
+    ////// //////// * /////// ///////
     blueClickOne = () => {
         // if one of the conditions is met below..
         if (this.state.colors[2] === 'purple' || this.state.colors[2] === 'gray' || this.state.colors[2] === 'black' || this.state.colors[2] === 'white'){
@@ -48,26 +51,44 @@ class PadContainer extends Component {
             this.setState(prevState => {
                 return {
         // pulling previous colors w/ prevState and keeps them the same while setting color @ colors[2] to blue
-                colors: [prevState.colors[0], prevState.colors[1], 'blue', prevState.colors[3] ]
+                colors: [prevState.colors[0], prevState.colors[1], 'blue', prevState.colors[3]]
                 }
             })
         }
-        // if condition is met
+        /////// if condition is met
         if (this.state.colors[2] === 'blue'){
         // sets state by..
             this.setState(prevState => {
                 return{
         // pulling previous colors w/ prevState and keeps them the same while setting color @ colors[2] to gray
-                colors: [prevState.colors[0], prevState.colors[1], 'gray', [prevState.colors[3]]]
+                colors: [prevState.colors[0], prevState.colors[1], 'gray', prevState.colors[3]]
                 }
             })
         }
     }
 
+    ////// //////// * /////// ///////
     blueClickTwo = () => {
-        this.setState({
-
-        })
+        // if one of the conditions is met below..
+        if (this.state.colors[3] === 'purple' || this.state.colors[3] === 'gray' || this.state.colors[3] === 'black' || this.state.colors[3] === 'white'){
+            // sets state by..
+                this.setState(prevState => {
+                    return {
+            // pulling previous colors w/ prevState and keeps them the same while setting color @ colors[2] to blue
+                    colors: [prevState.colors[0], prevState.colors[1], prevState.colors[2], 'blue']
+                    }
+                })
+            }
+            ////// if condition is met
+            if (this.state.colors[3] === 'blue'){
+            // sets state by..
+                this.setState(prevState => {
+                    return{
+            // pulling previous colors w/ prevState and keeps them the same while setting color @ colors[2] to gray
+                    colors: [prevState.colors[0], prevState.colors[1], prevState.colors[2], 'gray']
+                    }
+                })
+            }
     }
         
     render(){
@@ -84,32 +105,3 @@ class PadContainer extends Component {
 }
 
 export default PadContainer;
-
-// bwhandleClick = () => {
-//     if (this.state.color === '' || this.state.color === this.state.colors[1] || this.state.color === this.state.colors[2]){
-//         this.setState({
-//             color: this.state.colors[0]
-//         })
-//     } 
-//     if (this.state.color === this.state.colors[0] || this.state.color === this.state.colors[2]){
-//         this.setState({
-//             color: this.state.colors[1]
-//         })
-//     }
-// }
-
-
-// purpClick = () => {
-//     if(this.state.color[0] === 'b' || this.state.color[0] === 'w' || this.state.color === ''){
-//         this.setState({
-//                 color: 'gray',
-//                 colors: this.state.colors[2]
-//             })
-//         console.log(this.state.color)
-//         }
-//         if (this.state.color === 'purple'){
-//             this.setState({
-//                 color: ''
-//             })
-//         }
-//     }
