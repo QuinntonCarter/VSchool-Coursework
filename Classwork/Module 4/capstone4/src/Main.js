ec7cc4eed9da36a29c446775e4bb6978ea5b961d
quinntoncarter92 <quinntoncarter92@gmail.com>
2020-12-15 06:21:41 -0700

    working on capstone mod 4

--- Classwork/Module 4/capstone4/src/Main.js
+++ Classwork/Module 4/capstone4/src/Main.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { ThemeContextConsumer } from './ThemeContext'
+
 
 // for date retrieval and display
 var d = new Date()
 var day = d.getDay()
-var month = new Array();
+var month = [];
 month[0] = "January";
 month[1] = "February";
 month[2] = "March";
@@ -20,19 +21,46 @@ month[10] = "November";
 month[11] = "December";
 var n = month[d.getMonth()];
 
-function Main(){
-    return(
-        <div className='main'>
-            {/* <ThemeContextConsumer> */}
-                {/* {context =>( */}
-                    <div>
-                        <h1 style={{textAlign: 'left', padding: '15px', fontSize: '72px'}}> {day} </h1>
-                        <h2 style={{fontSize: '35px'}}> {n} </h2>
-                    </div>
-                {/* )} */}
-            {/* </ThemeContextConsumer> */}
-        </div>
-    )
+// api retrieval 
+
+
+class Main extends Component {
+    state = {
+        daily: {}
+    }
+
+    // api get //
+    componentDidMount(){
+        fetch("https://api.openweathermap.org/data/2.5/weather?id=5780993&appid=5ae7b3c76c2e696e51c9f8585a68c324")
+        // fetch("https://api.openweathermap.org/data/2.5/onecall?lat=40.767&lon=-111.8904&exclude=minutely&appid=5ae7b3c76c2e696e51c9f8585a68c324")
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                daily: data
+            })
+            console.log(data)
+        })
+    }
+
+    render(){
+        const temp = this.state.daily?.main?.temp
+        return(
+            <div className='main'>
+                {/* <ThemeContextConsumer> */}
+                    {/* {context =>( */}
+                        <div>
+                            <h1 style={{padding: '15px'}}> {day} {n} </h1>
+                            <h2> {this.state.daily.name} </h2>
+                            <h1> {temp}{`º`} </h1>
+                            <h3> {this.state.daily?.weather?.[0]?.description} </h3>
+                            <h4>{`humidity`} {this.state.daily?.main?.humidity}{`%`} </h4> 
+                            
+                        </div>
+                    {/* )} */}
+                {/* </ThemeContextConsumer> */}
+            </div>
+        )
+    }
 }
 
 export default Main
