91a14faceac7bc3e9d761e4a2a85075b34f0a257
quinntoncarter92 <quinntoncarter92@gmail.com>
2021-02-08 20:56:24 -0700

    finished thing finder

--- Classwork/module 5/2thingfinder/routes/appleRouter.js
+++ Classwork/module 5/2thingfinder/routes/appleRouter.js
@@ -3,14 +3,14 @@ const appleRouter = express.Router()
 const { v4: uuidv4 } = require('uuid')
 
 const apples = [
-    {name: 'Fuji', taste: 'sweet', price: 1, _id: uuidv4()},
-    {name: 'Envy', taste: 'semi-sweet', price: 2, _id: uuidv4()},
-    {name: 'Ambrosia', taste: 'semi-sweet', price: 2, _id: uuidv4()},
-    {name: 'Gala', taste: 'semi-sweet', price: 1, _id: uuidv4()},
-    {name: 'Opal', taste: 'semi-tart', price: 1, _id: uuidv4()},
-    {name: 'Jazz', taste: 'semi-tart', price: 2, _id: uuidv4()},
-    {name: 'Pink Lady', taste: 'semi-tart', price: 2, _id: uuidv4()},
-    {name: 'Granny Smith', taste: 'tart', price: 1, _id: uuidv4()}
+    {name: 'Fuji', taste: 'sweet', price: '.50', _id: uuidv4()},
+    {name: 'Envy', taste: 'semi-sweet', price: '5', _id: uuidv4()},
+    {name: 'Ambrosia', taste: 'semi-sweet', price: '2', _id: uuidv4()},
+    {name: 'Gala', taste: 'semi-sweet', price: '.50', _id: uuidv4()},
+    {name: 'Opal', taste: 'semi-tart', price: '1', _id: uuidv4()},
+    {name: 'Jazz', taste: 'semi-tart', price: '5', _id: uuidv4()},
+    {name: 'Pink Lady', taste: 'semi-tart', price: '2', _id: uuidv4()},
+    {name: 'Granny Smith', taste: 'tart', price: '1', _id: uuidv4()}
 ]
 
 appleRouter.route('/')
@@ -25,6 +25,12 @@ appleRouter.route('/')
         apples.push(newApple)
         res.send(`Successfully added ${newApple.name} to the database!`)
     })
+    // get an apple
+appleRouter.get('/:appleId', (req,res) => {
+    const appleId = req.params.appleId
+    const foundApple = apples.find(apple => apple._id === appleId)
+    res.send(foundApple)
+})
 
     // delete an apple
 appleRouter.delete('/:appleId', (req,res) => {
@@ -35,13 +41,19 @@ appleRouter.delete('/:appleId', (req,res) => {
 })
 
     // get by taste
-appleRouter.get(':searchBy', (req, res) => {
-    const taste = req.query.taste
-    const foundTaste = apples.filter(apple => apple.taste === taste)
-    // returns by taste
-    res.send(foundTaste)
-    // how to implement search by min and max price?
-    // const price = req.query.price
+appleRouter.get('/search/:searchBy', (req, res) => {
+
+    if(req.params.searchBy === 'price'){
+        const min = req.query.min
+        const max = req.query.max
+        const foundPrice = apples.filter(apple => apple.price <= max && apple.price >= min)
+        res.send(foundPrice)
+    } else if (req.params.searchBy === 'taste'){
+        const taste = req.query.taste
+        const foundTaste = apples.filter(apple => apple.taste === taste)
+        res.send(foundTaste)
+    }
+
     res.send(req)
 })
 
