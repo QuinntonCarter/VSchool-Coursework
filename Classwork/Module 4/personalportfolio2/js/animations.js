37800ec66e0c18a38e6afffdf66dc1dd65cb4a21
quinntoncarter92 <quinntoncarter92@gmail.com>
2021-02-10 13:13:32 -0700

    .

--- Classwork/Module 4/personalportfolio2/js/animations.js
+++ Classwork/Module 4/personalportfolio2/js/animations.js
@@ -1,13 +1,19 @@
 $(document).ready(()=> {
 
+    $('aboutTitle').hide()
     $('#about').hide()
+
+    $('#contactTitle').hide()
     $('#contact').hide()
+
     $('#navCircle').on('click', () => {
         $('#navCircle').animate({
             width: '1000px',
             height: '1000px'
         }, 1500);
-        $('#about').fadeToggle(2200)
+        $('aboutTitle').fadeToggle(2200);
+        $('#about').fadeToggle(2200);
+        $('#contactTitle').fadeToggle(2200)
         $('#contact').fadeToggle(2200)
     })
 
@@ -16,9 +22,11 @@ $(document).ready(()=> {
             height: '30px',
             width: '30px'
         }, 1500);
+        $('#aboutTitle').fadeToggle(1700)
         $('#about').fadeToggle(1700)
+
+        $('#contactTitle').fadeToggle(1700)
         $('#contact').fadeToggle(1700)
     })
-
 
 })
