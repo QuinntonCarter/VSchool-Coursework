5e241e48d7bbe3bd06830644c953f8cac02d05ae
quinntoncarter92 <quinntoncarter92@gmail.com>
2021-02-18 14:39:52 -0700

    .

--- Classwork/module 5/4todobackend/server.js
+++ Classwork/module 5/4todobackend/server.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/todos', require('./routes/todoRouterTwo.js'))
+app.use('/todos', require('./routes/todoRouter.js'))
 
 app.listen(8500, () => {
     console.log("< server running on 8500 >")
