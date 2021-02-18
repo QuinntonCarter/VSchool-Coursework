c486afab945f5f9e4a4fbe2e1e0ac4e835176d4f
quinntoncarter92 <quinntoncarter92@gmail.com>
2021-02-18 14:51:47 -0700

    finished todo backend

--- Classwork/module 5/4todobackend/routes/todoRouter.js
+++ Classwork/module 5/4todobackend/routes/todoRouter.js
@@ -8,13 +8,13 @@ const todos = [
 description: 'wakwtfgo GLUG',
 imageUrl: 'https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/foodnavigator-latam.com/article/2018/10/22/guayaki-eyes-yerba-mate-expansion-with-organic-and-sustainable-messaging/8748424-1-eng-GB/Guayaki-eyes-yerba-mate-expansion-with-organic-and-sustainable-messaging.jpg',
 completed: true,
-_id: '1337h4ck3rl0l'
+_id: uuidv4()
 }, 
 {name: 'eat food',
 description: 'something healthy without too much sugar',
 imageUrl: 'https://californiaavocado.com/wp-content/uploads/2020/07/California-Avocado-Toast-Three-Ways.jpeg',
 completed: true,
-_id: '347f00dn00b'
+_id: uuidv4()
 }
 ]
 
