801114e005571b2f120a0df27503fc03da57c544
quinntoncarter92 <quinntoncarter92@gmail.com>
2021-03-16 17:06:07 -0600

    crudstore rebuild

---
+++ Classwork/module 5/crudstore/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+mongoose.connect('mongodb://localhost:27017/storedb',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+)
+.then(() => console.log('Connected to the Crud Store Database!'))
+.catch(err => console.log(err))
+
+app.use('/inventory', require('./routes/inventoryRouter.js'))
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
+
+app.listen(5000, () => {
+    console.log('The server is running on port 5000!!')
+})
