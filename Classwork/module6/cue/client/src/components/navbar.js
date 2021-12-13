769adc187914b1a0e92e2971207a125ac560be88
QuinntonCarter <qdcarter92@protonmail.com>
2021-12-13 10:26:03 -0700

    fixed rendering issue w search

--- Classwork/module6/cue/client/src/components/navbar.js
+++ Classwork/module6/cue/client/src/components/navbar.js
@@ -8,17 +8,17 @@ export const Navbar = () => {
         logout
     } = useContext(UserContext);
     const {
+        found,
         setFound
     } = useContext(AppContext);
-    var location = useLocation();
     // resets found to nothing to prevent rendering issues between search results
-    const conditionalReset = (location) => {location.pathname.includes('/results') && setFound('')}
+    const conditionalReset = (found) => {found && setFound('')}
     
     return(
         <div className='flex flex-row justify-evenly fixed bottom-0 w-screen bg-navy-800'>
             <Link className='w-full p-3 pl-5' to='/recent_mood_artists'> <button> profile </button> </Link>
             <Link className='w-full p-3' to='/lists'> <button> friends </button> </Link>
-            <Link className='w-full p-3' to='/search' onClick={() => conditionalReset(location)}> <button> search </button> </Link>
+            <Link className='w-full p-3' to='/search' onClick={() => conditionalReset(found)}> <button> search </button> </Link>
             <button className='bg-cerise-700 w-full p-1' onClick={logout}> logout </button>
         </div>
     )
