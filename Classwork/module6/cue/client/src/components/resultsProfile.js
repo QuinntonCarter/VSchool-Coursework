55764532b35ec38d44074aed84e42dba0f6b8005
QuinntonCarter <qdcarter92@protonmail.com>
2021-12-12 19:05:44 -0700

    ... bugs

--- Classwork/module6/cue/client/src/components/resultsProfile.js
+++ Classwork/module6/cue/client/src/components/resultsProfile.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { AppContext } from './context/appContext';
 
+// ** when navigating to search from here, found returns not iterable and crashes app
 export const ResultsProfile = () => {
     const {
         getTracksFeatures,
@@ -18,7 +19,7 @@ export const ResultsProfile = () => {
     return(
         <div className='container-main '>
             <button onClick={() => history.goBack()} className='btnbold-small bg-cerise-700 text-sm'> go back </button>
-            <div className='container-main text-center mb-0 h-screen bg-navy-700 rounded'>
+            <div className='container-main text-center bg-navy-700 rounded'>
                 <a className='text-4xl p-3' href={`https://open.spotify.com/playlist/${found.id}`}> {found.name} </a>
                 {mappedTracks}
                 <button onClick={() => getTracksFeatures()} className='btn bg-indigo-700 mt-7'> analyze tracks </button>
